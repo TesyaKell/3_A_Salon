2528171ca2f6a975547ecc5b710173ea7ac2730a
Imanuella-Daniel <220711901@students.uajy.ac.id>
2024-09-11 10:42:34 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,6 @@ class MainApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: Text('Redempta Rista Elvira - 247000675'),
-          child: Text('arthi-247000676S!'),
-          child: Text('Gabriella-220711884'),
-          child: Text('Bernadeta-220711678'),
-          child: Text('Tesya-220711896'),
-          child: Text(' Imanuella Daniel  - 220711901'),
-
         ),
       ),
     );
