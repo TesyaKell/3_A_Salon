3180086fdc91925b223e00c9a2ff6fa29a7f01b2
elarthi <helearthi@gmail.com>
2024-09-11 10:14:54 +0700

    ubah_elarthi

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('arthi-247000676'),
+          child: Text('arthi-247000676S!'),
         ),
       ),
     );
