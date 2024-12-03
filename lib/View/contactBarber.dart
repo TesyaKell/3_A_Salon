ca434a8fe09d597b53c940f4d3dbf6c168196fbc
Imanuella Daniel <imanuelladaniel@gmail.com>
2024-12-03 22:14:27 +0700

    Menambahkan Detail Barber

--- lib/View/contactBarber.dart
+++ lib/View/contactBarber.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:a_3_salon/View/SalonBarberDetailPage.dart';
 
 class ContactBarbersPage extends StatefulWidget {
   @override
@@ -10,68 +9,102 @@ class ContactBarbersPage extends StatefulWidget {
 }
 
 class _ContactBarbersPageState extends State<ContactBarbersPage> {
-  List<dynamic> barbers = [];
+  List<dynamic> barbersWithDetails = [];
   bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    tampilBarber();
+    fetchBarbersWithDetails();
   }
 
-  Future<void> tampilBarber() async {
+  Future<void> fetchBarbersWithDetails() async {
     try {
-      final response =
-          await http.get(Uri.parse('http://192.168.1.17:8000/api/barbers'));
+      final response = await http
+          .get(Uri.parse('http://192.168.1.17:8000/api/detail_layanans'));
       if (response.statusCode == 200) {
         setState(() {
-          barbers = json.decode(response.body);
+          barbersWithDetails = json.decode(response.body);
           isLoading = false;
         });
       } else {
         throw Exception('Failed to load barbers');
       }
     } catch (e) {
-      print(e);
       setState(() {
         isLoading = false;
       });
     }
   }
 
-  void _launchWhatsApp(String phone) async {
-    final url = Uri.parse('https://wa.me/$phone');
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url, mode: LaunchMode.externalApplication);
-    } else {
-      throw 'Could not launch $url';
-    }
+  void _navigateToBarberDetailPage(dynamic barber) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SalonBarberDetailPage(barber: barber),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xFFFF4081),
         title: Text('Contact Barbers'),
+        centerTitle: true,
       ),
       body: isLoading
           ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: barbers.length,
+          : GridView.builder(
+              padding: const EdgeInsets.all(10),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                childAspectRatio: 0.8,
+              ),
+              itemCount: barbersWithDetails.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      'http://192.168.1.17:8000/storage/${barbers[index]['foto']}',
+                final barber = barbersWithDetails[index];
+                return GestureDetector(
+                  onTap: () => _navigateToBarberDetailPage(barber),
+                  child: Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(12),
+                          child: barber['foto'] != null
+                              ? Image.network(
+                                  'http://192.168.1.17:8000/storage/${barber['foto']}',
+                                  height: 100,
+                                  width: 100,
+                                  fit: BoxFit.cover,
+                                )
+                              : Image.asset(
+                                  'assets/default_image.png',
+                                  height: 100,
+                                  width: 100,
+                                  fit: BoxFit.cover,
+                                ),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          barber['nama_barber'] ?? 'Nama tidak tersedia',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  title: Text(barbers[index]['nama_barber']),
-                  subtitle: Text(barbers[index]['kontak']),
-                  trailing:
-                      Icon(FontAwesomeIcons.whatsapp, color: Colors.green),
-                  onTap: () {
-                    _launchWhatsApp(barbers[index]['kontak']);
-                  },
                 );
               },
             ),
